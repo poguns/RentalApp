e53e0c9aef2c6a0320e941942a949a7650889fd8
poguns_ <praiseoguntibeju@gmail.com>
2026-05-04 13:22:24 +0000

    location tests

--- StarterApp.Tests/Services/StarterAppServices/Services/ILocationService.cs
+++ StarterApp.Tests/Services/StarterAppServices/Services/ILocationService.cs
@@ -2,8 +2,7 @@ namespace StarterApp.Services;
 
 public interface ILocationService
 {
-    Task<Location?> GetCurrentLocationAsync();
-    Task<double> GetDistanceKmAsync(double lat1, double lon1,
-                                    double lat2, double lon2);
+    Task<(double Latitude, double Longitude)?> GetCurrentLocationAsync();
+    Task<double> GetDistanceKmAsync(double lat1, double lon1, double lat2, double lon2);
     bool IsLocationAvailable();
 }
